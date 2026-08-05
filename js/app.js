9b40d314efe8ce0c86140bc88c8a2357c7376647
AhKhamis <7mody.stacks@gmail.com>
2026-08-05 17:43:07 +0300

    completed matching the two cards & cannot select more than two cards

--- js/app.js
+++ js/app.js
@@ -1,9 +1,68 @@
 const allCards = document.querySelectorAll('.card');
 
+let firstCard = null;
+let secondCard = null;
+
+let canClick = true;
+let score = 0;
+
+
 allCards.forEach(card => {
     card.addEventListener('click', handleCardClick);
 });
 
-function handleCardClick(event) {
+function handleCardClick() {
+    if(!canClick) return;
+
+    if(this.classList.contains('flip')) return;
+
+
     this.classList.add('flip');
+
+    if (!firstCard) {
+  firstCard = this;
+  return;
+}
+
+secondCard = this;
+
+
+    const img1 = firstCard ? firstCard.firstElementChild.src : null;
+    const img2 = secondCard ? secondCard.firstElementChild.src : null;
+
+    if(img1 === img2){
+    
+    firstCard = null;
+    secondCard = null;
+
+    score++;
+
+    if(score === 6){
+        handleGameOver();
+
+    }
+    return;
+}
+
+    else if(img1 && img2){
+
+        canClick = false;
+
+        setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        firstCard = null;
+        secondCard = null;
+
+        canClick = true;
+    },1000);
+}
+function handleGameOver() {
+    setTimeout(() => {
+        let playAgain = confirm('Game Over! Do you want to play again?');
+        if(playAgain === true) {
+        location.reload();
+        }
+    }, 1000);
+}
 }
